df584b95de474b795bc8327f02f6f44b32df8895
Tomasz Bałdyga <268964@pw.edu.pl>
2019-11-08 09:52:33 +0100

    lab 5

--- lab_5/tasks/task_1.py
+++ lab_5/tasks/task_1.py
@@ -7,6 +7,7 @@ Podnoszone wyjątki:
 jak i operację z argumentem domyślnym) - EmptyMemory
 - dzielenie przez zero jest przekształcane w CalculatorError
 """
+#mhm
 from operator import add, mul, sub, truediv
 
 
